7b09b5e4bf5b4f9ad5c7d9a705a9a4ed98e65b11
Mariam Saab <marimsaab1983@gmail.com>
2019-07-30 18:30:55 +0300

    calculator

---
+++ app/src/main/java/com/ru/oturisme/calculator/OperationType.java
@@ -0,0 +1,8 @@
+package com.ru.oturisme.calculator;
+
+public enum OperationType {
+    ADD,
+    SUBTRACT,
+    MULTIPLY,
+    DIVIDE
+}
---
+++ app/src/main/java/com/ru/oturisme/calculator/Symbol.java
@@ -0,0 +1,7 @@
+package com.ru.oturisme.calculator;
+
+public enum Symbol {
+    FIRST_DIGIT,
+    OPERATION,
+    SECOND_DIGIT
+}
